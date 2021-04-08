40bdfa8308d9eb7aa0875f8a6254a09765e9f906
Dmitriy <svsdima1995@mail.ru>
2021-04-08 12:31:35 +0300

    Menu. Swiper-Slider. Header. Boost.

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    new Swiper('.swiper-container', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
